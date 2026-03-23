b7e3f236129fe6e68797be72ffded7c6b8d39c50
Nour1717 <elabed.nour@gmail.com>
2026-03-23 23:15:40 +0100

    update server.js

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.use(errorHandler);
 connectDB();
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
-    console.log(`🌍 Accepting requests from: ${CLIENT_URL}`);
+    console.log(`🌍 Accepting requests from: ${CLIENT_URL}`); // client_url
 });
